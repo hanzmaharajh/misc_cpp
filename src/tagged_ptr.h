93946cabc52c6c132828d40333040bffaad9a19e
Hanz Maharajh <hanzmaharajh@gmail.com>
2024-09-23 08:54:05 -0300

    Variant tagged ptr - First

--- src/tagged_ptr.h
+++ src/tagged_ptr.h
@@ -2,13 +2,14 @@
 #include <climits>
 
 #include "log2.h"
+#include "pack_manipulation.h"
 
 namespace misc {
 template <typename T, size_t Align = alignof(T)>
 class tagged_ptr {
   // TODO Use log2() when it is constexpr
   [[nodiscard]] static constexpr size_t num_bits(size_t v) {
-    return v < 2 ? v : 1 + num_bits(v >> 1);
+    return v == 0 ? 0 : (v == 1 ? 1 : 1 + num_bits(v >> 1));
   }
 
  public:
@@ -41,9 +42,9 @@ class tagged_ptr {
     assert((reinterpret_cast<uintptr_t>(ptr) & tag_mask) == 0);
     m_ptr = (m_ptr & tag_mask) | reinterpret_cast<uintptr_t>(ptr);
   }
+
   T* operator->() const { return get(); }
 };
-
 
 template <typename T, size_t Align>
 class tagged_ptr<T[], Align> : private tagged_ptr<T, Align> {
@@ -116,6 +117,64 @@ class unique_tagged_ptr<T[]> : private tagged_ptr<T[]> {
   void reset(T* ptr = nullptr) {
     delete get();
     base_type::reset(ptr);
+  }
+};
+
+template <size_t Align, typename... Types>
+class variant_tagged_ptr : private tagged_ptr<char, Align> {
+  using base_type = tagged_ptr<char, Align>;
+
+  template <typename Visitor, size_t Ind>
+  auto visit(Visitor&& visitor, std::integer_sequence<size_t, Ind>) const {
+    return visitor(get_as<std::tuple_element_t<Ind, std::tuple<Types...>>>());
+  }
+
+  template <typename Visitor, size_t Ind, size_t... Inds>
+  auto visit(Visitor&& visitor,
+             std::integer_sequence<size_t, Ind, Inds...>) const {
+    if (Ind == index())
+      return visitor(get_as<std::tuple_element_t<Ind, std::tuple<Types...>>>());
+    return visit(std::forward<Visitor>(visitor),
+                 std::integer_sequence<size_t, Inds...>{});
+  }
+
+ public:
+  static_assert(sizeof...(Types) <= base_type::tag_mask);
+
+  // TODO Should I use this check? It's more limiting. Not using it is more
+  // dangerous, but maybe this is an advanced class that users should be aware
+  // on their own?
+  static_assert(((alignof(Types) <= Align) && ...));
+
+  template <typename Type>
+  variant_tagged_ptr(Type* ptr)
+      : base_type(reinterpret_cast<char*>(ptr), index_of_v<Type, Types...>) {}
+
+  template <typename Type>
+  [[nodiscard]] Type* get_as() const {
+    if (base_type::tag() != index_of_v<Type, Types...>) {
+      return nullptr;
+    }
+    return reinterpret_cast<Type*>(base_type::get());
+  }
+
+  template <typename Type>
+  void reset(Type* ptr) {
+    base_type::reset(reinterpret_cast<char*>(ptr));
+    base_type::set_tag(index_of_v<Type, Types...>);
+  }
+
+  void reset(nullptr_t) {
+    base_type::reset(nullptr);
+    base_type::set_tag(0);
+  }
+
+  size_t index() const { return base_type::tag(); }
+
+  template <typename Visitor>
+  auto visit(Visitor&& visitor) const {
+    return visit(std::forward<Visitor>(visitor),
+                 std::make_index_sequence<sizeof...(Types)>{});
   }
 };
 }  // namespace misc
--- test/tagged_ptr_test.cpp
+++ test/tagged_ptr_test.cpp
@@ -1,6 +1,7 @@
 #include <gtest/gtest.h>
 #include <tagged_ptr.h>
 
+#include "overloaded.h"
 #include "test.h"
 
 TEST(tagged_ptr, Construct) {
@@ -73,4 +74,53 @@ TEST_F(UniqueTaggedPtrCountingFixture, DestroyArr) {
     misc::unique_tagged_ptr<TestElement[]> tagged{ptr, 0x07};
   }
   ASSERT_EQ(call_counts.destructor_calls, 10);
+}
+
+namespace {
+class alignas(8) A {
+ public:
+  int a;
+};
+class B : public A {};
+class Bb : public B {};
+class C : public A {};
+class D : public A {};
+
+}  // namespace
+
+TEST(variant_tagged_ptr, variant_tagged_ptr) {
+  using test_tagged_ptr = misc::variant_tagged_ptr<alignof(A), A, B, Bb, C, D>;
+  B b;
+  Bb bb;
+
+  test_tagged_ptr ptr(&b);
+
+  {
+    B* b_ptr = ptr.get_as<B>();
+    C* c_ptr = ptr.get_as<C>();
+    EXPECT_EQ(b_ptr, &b);
+    EXPECT_EQ(c_ptr, nullptr);
+  }
+
+  ptr.reset(&bb);
+  {
+    Bb* bb_ptr = ptr.get_as<Bb>();
+    C* c_ptr = ptr.get_as<C>();
+    EXPECT_EQ(bb_ptr, &bb);
+    EXPECT_EQ(c_ptr, nullptr);
+  }
+
+  {
+    auto r = ptr.visit(misc::overloaded{[](const A*) { return 1; },
+                                        [](const Bb*) { return 3; }});
+
+    EXPECT_EQ(r, 3);
+  }
+
+  {
+    auto r = ptr.visit(misc::overloaded{[](const A*) { return 1; },
+                                        [](const B*) { return 3; }});
+
+    EXPECT_EQ(r, 3);
+  }
 }
